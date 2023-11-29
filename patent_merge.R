3944001facbeff5306b475240f41f5aeaef16d26
Allegra Saggese <111875712+allegra-saggese-econ@users.noreply.github.com>
2023-11-29 14:12:22 +0000

    download sqldf to read in patstat
    - patstat files are large, need to be read in differently than normal CSVs

--- patent_merge.R
+++ patent_merge.R
@@ -1,2 +1,16 @@
 # merge PATSTAT data
 
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+library(readr)
+library(sqldf)
+library(R.utils)
+
+setwd("C:/Users/SAGGESE/Documents/GitHub/amc_ccs/data/PATSTAT")
+temp = list.files(pattern="\\.csv$")
+for (i in 1:length(temp)) assign(temp[i], read.csv(temp[i]))
+
+file = paste0(getwd(), "/tls226_part02.csv")
+data.table.timing <- system.time(allData <- read.csv(file)) # test the time to read in the PATSTAT
+
