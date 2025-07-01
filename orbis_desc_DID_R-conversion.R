5c7361fcd7cd3ee903713392c0780327207b46d4
Allegra Saggese <111875712+allegra-saggese-econ@users.noreply.github.com>
2025-07-01 13:09:16 +0200

    orbis desc conversion - load in files
    - load in necessary files to redo the orbis description / RDID / SDID
    - began with fuzzy match on firm names and patent data

--- orbis_desc_DID_R-conversion.R
+++ orbis_desc_DID_R-conversion.R
@@ -3,6 +3,7 @@
 #' Last updated: `r format(Sys.Date(), "%Y-%m-%d")`
 
 # Load necessary libraries
+library(stringdist)
 library(dplyr)
 library(tidyr)
 library(lubridate)
@@ -21,19 +22,60 @@ if (username == "ignaciobanaressanchez") {
   projdir <- "/Users/allegrasaggese/Documents/GitHub/amc_ccs/"
 }
 
-
-
+####### IMPORT DATA SETS ################
 # Import Orbis data - updated with clean data
 orbis_data <- read_csv(file.path(projdir, "clean_outputs/orbis_long_2025-06-26.csv"))
 
-# Import patent<>firm matched data 
-##### needs formatting work 
-patent_data <- read_csv(file.path(projdir, "clean_outputs/did_df.csv"))
+# Import patent<>firm matched data (do from scratch - take OG dataset) 
+patent_data_raw <- read_csv(file.path(projdir, "clean_outputs/firm_level_data.csv")) # does not include treatment variavble 
+
+# Import cleaned version of patent <> firm matched data
+patent_data_clean <- read_csv(file.path(projdir, "clean_outputs/did_df_copy.csv"))
+
+# before we merge orbis to patent data -- make orbis data month-year
+months_df <- tibble(Month = 1:12)
+orbis_month <- orbis_data %>%
+  tidyr::crossing(months_df) %>%
+  arrange(firm_name, Year, Month)
+
+head(orbis_month, 20)
+
+orbis_month <- orbis_month %>%
+  mutate(
+    m_y = format(as.Date(paste(Year, Month, "01", sep = "-")), "%b %Y")
+  )
+
+# extract out firm name in patent_data_clean
+patent_f <- patent_data_clean %>%
+  mutate(firm_name = substr(key, 1, nchar(key) - 5))
+
+############ FIRM NAME MATCH #################
+# WILL NEED TO CLEAN UP BOTH SETS OF NAMES HERE for a fuzzy match
+
+# step 1 - review the existing matches 
+names_1 <- unique(patent_f$firm_name) # check to see if there are any clear matches
+names_2 <- unique(orbis_month$firm_name)
+
+only_in_df1 <- setdiff(names_1, names_2)
+only_in_df2 <- setdiff(names_2, names_1)
+
+comparison_result <- list(
+  only_in_df1 = only_in_df1,
+  only_in_df2 = only_in_df2
+)
+
+common_names <- intersect(names_1, names_2) # no common names - need to ignore case! 
+# ignoring case theres only two that match perfectly 
+common_names <- intersect(tolower(names_1), tolower(names_2)) 
+
+# without clear matches - use FUZZY MATCH 
 
 
-# Keep relevant columns
-orbis_data <- orbis_data %>%
-  select(applicant_name, file_date, treat_date)
+
+# Merge orbis and clean patent data
+fdf <- full_join(orbis_month, patent_f, by = c("m_y, firm_name"))
+
+
 
 # Convert file_date to Date and extract year and month
 orbis_data <- orbis_data %>%
@@ -42,11 +84,10 @@ orbis_data <- orbis_data %>%
          f_month = month(file_date, label = TRUE, abbr = FALSE),
          f_modate = as.yearmon(file_date))
 
-# Drop rows with NA in f_modate
-orbis_data <- orbis_data %>% drop_na(f_modate)
+
 
 # Generate patent filed variable
-orbis_data <- orbis_data %>%
+fulldf <- f_df %>%
   mutate(patent_filed = 1)
 
 # Collapse data
